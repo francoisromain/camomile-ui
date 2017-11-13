fe2676a29f174676118277d93bb059e1ee85e1b2
Francois Romain <francoisromain@gmail.com>
2017-11-13 11:32:42 +0100

    fix: cleanup

--- src/components/camomile/groups.vue
+++ src/components/camomile/groups.vue
@@ -2,7 +2,7 @@
   <div v-if="adminIs">
     <div class="flex flex-start">
       <h2 class="mt-s">Groups</h2>
-      <button @click="popupOpen({ type: 'groups', id: null, closeBtn: true, title: 'Add group', component: popupEdit })" class="flex-right btn p-s"><i class="icon-24 icon-24-plus"></i></button>
+      <button @click="popupOpen({ id: null, ...popupEditConfig })" class="flex-right btn p-s"><i class="icon-24 icon-24-plus"></i></button>
     </div>
     <div>
       <table class="table mb-0">
@@ -13,9 +13,9 @@
           <td>{{ group.name }}</td>
           <td>{{ group.userIds.length }}</td>
           <td class="text-right">
-            <button @click="popupOpen({ id: group.id, closeBtn: true, title: 'Group users', component: popupGroupUsers })" class="btn px-s py-s my--s h5">Users</button>
-            <button @click="popupOpen({ type: 'groups', id: group.id, closeBtn: true, title: 'Edit group', component: popupEdit })" class="btn px-s py-s my--s h5">Edit</button>
-            <button @click="popupOpen({ type: 'groups', id: group.id, closeBtn: true, title: 'Remove group', component: popupRemove })" class="btn px-s py-s my--s h5">Remove</button>
+            <button @click="popupOpen({ id: group.id, ...popupUsersConfig })" class="btn px-s py-s my--s h5">Users</button>
+            <button @click="popupOpen({ id: group.id, ...popupEditConfig })" class="btn px-s py-s my--s h5">Edit</button>
+            <button @click="popupOpen({ id: group.id, ...popupRemoveConfig })" class="btn px-s py-s my--s h5">Remove</button>
           </td>
         </tr>
       </table>
@@ -27,15 +27,29 @@
 import { mapMutations, mapActions, mapState } from 'vuex'
 import popupEdit from './utils/popup-edit.vue'
 import popupRemove from './utils/popup-remove.vue'
-import popupGroupUsers from './groups/popup-users.vue'
+import popupUsers from './groups/popup-users.vue'
 
 export default {
   name: 'camomile-groups',
   data () {
     return {
-      popupEdit,
-      popupRemove,
-      popupGroupUsers
+      popupRemoveConfig: {
+        type: 'groups',
+        closeBtn: true,
+        title: 'Remove group',
+        component: popupRemove
+      },
+      popupEditConfig: {
+        type: 'groups',
+        closeBtn: true,
+        title: 'Edit group',
+        component: popupRemove
+      },
+      popupUsersConfig: {
+        closeBtn: true,
+        title: 'Group users',
+        component: popupUsers
+      }
     }
   },
   computed: {
--- src/components/camomile/users.vue
+++ src/components/camomile/users.vue
@@ -2,7 +2,7 @@
   <div v-if="adminIs">
     <div class="flex flex-start">
       <h2 class="mt-s">Users</h2>
-      <button @click="popupOpen({ type: 'users', id: null, closeBtn: true, title: 'Add user', component: popupEdit })" class="flex-right btn p-s"><i class="icon-24 icon-24-plus"></i></button>
+      <button @click="popupOpen({ id: null, ...popupEditConfig })" class="flex-right btn p-s"><i class="icon-24 icon-24-plus"></i></button>
     </div>
     <div>
       <table class="table mb-0">
@@ -13,9 +13,9 @@
           <td>{{ user.name }}</td>
           <td>{{ user.role }}</td>
           <td class="text-right">
-            <button @click="popupOpen({ id: user.id, closeBtn: true, title: 'User groups', component: popupUserGroups })" class="btn px-s py-s my--s h5">Groups</button>
-            <button @click="popupOpen({ type: 'users', id: user.id, closeBtn: true, title: 'Edit user', component: popupEdit })" class="btn px-s py-s my--s h5">Edit</button>
-            <button @click="popupOpen({ type: 'users', id: user.id, closeBtn: true, title: 'Remove user', component: popupRemove })" class="btn px-s py-s my--s h5">Remove</button>
+            <button @click="popupOpen({ id: user.id, ...popupGroupsConfig })" class="btn px-s py-s my--s h5">Groups</button>
+            <button @click="popupOpen({ id: user.id, ...popupEditConfig })" class="btn px-s py-s my--s h5">Edit</button>
+            <button @click="popupOpen({ id: user.id, ...popupRemoveConfig })" class="btn px-s py-s my--s h5">Remove</button>
           </td>
         </tr>
       </table>
@@ -27,15 +27,29 @@
 import { mapMutations, mapActions, mapState } from 'vuex'
 import popupEdit from './utils/popup-edit.vue'
 import popupRemove from './utils/popup-remove.vue'
-import popupUserGroups from './users/popup-groups.vue'
+import popupGroups from './users/popup-groups.vue'
 
 export default {
   name: 'camomile-users',
   data () {
     return {
-      popupEdit,
-      popupRemove,
-      popupUserGroups
+      popupEditConfig: {
+        type: 'users',
+        closeBtn: true,
+        title: 'Edit user',
+        component: popupEdit
+      },
+      popupGroupsConfig: {
+        closeBtn: true,
+        title: 'User groups',
+        component: popupGroups
+      },
+      popupRemoveConfig: {
+        type: 'users',
+        closeBtn: true,
+        title: 'Remove user',
+        component: popupRemove
+      }
     }
   },
   computed: {
