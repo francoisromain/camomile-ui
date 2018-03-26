caf23332b8cd1fc3b4734947a3e26d690e53d32e
Francois Romain <francoisromain@gmail.com>
2018-03-26 22:09:40 +0200

    feat: minified version of the umd bundle

--- build/rollup.config.dist.js
+++ build/rollup.config.dist.js
@@ -5,6 +5,7 @@ import postcss from 'rollup-plugin-postcss'
 import json from 'rollup-plugin-json'
 import builtins from 'rollup-plugin-node-builtins'
 import buble from 'rollup-plugin-buble'
+import uglify from 'rollup-plugin-uglify'
 import pkg from '../package.json'
 
 export default [
@@ -35,6 +36,36 @@ export default [
       buble({
         objectAssign: 'Object.assign'
       })
+    ]
+  },
+  {
+    input: 'src/app.js',
+    external: ['vue', 'vuex', 'camomile-client'],
+    output: {
+      file: 'dist/camomile-ui.min.js',
+      format: 'umd',
+      name: 'camomile-ui.min',
+      globals: {
+        'camomile-client': 'Camomile',
+        vuex: 'Vuex',
+        vue: 'Vue'
+      }
+    },
+    plugins: [
+      resolve({
+        preferBuiltins: true
+      }),
+      commonjs(),
+      builtins(),
+      json(),
+      vue({
+        css: true
+      }),
+      postcss(),
+      buble({
+        objectAssign: 'Object.assign'
+      }),
+      uglify()
     ]
   },
   {
